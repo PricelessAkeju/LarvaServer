e72236015c731a7dc35a7a00e9cff94a21e4108d
PricelessAkeju <akejutunjis@gmail.com>
2024-09-12 01:32:01 +0100

    attendanceMCR

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import tutorModel from '../server/MODELS/tutorModel.js';
+import studentModel from '../server/MODELS/studentModel.js';
+import attendanceModel from "./MODELS/attendanceModel.js";
+import tutorRouter from "./ROUTERS/tutorRouter.js";
+import studentRouter from "./ROUTERS/studentRouter.js";
+import attendanceRouter from "./ROUTERS/attendanceRouter.js";
+
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+app.use(tutorRouter)
+app.use(studentRouter)
+app.use(attendanceRouter)
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT
+
+mongoose.connect (process.env.MONGO_URI)
+   .then(()=> {
+    app.listen(port, ()=> {
+        console.log(`server listen on ${port}`)
+    })
+    console.log("connected to MongoDB")
+    })
+   .catch(err=> console.error(err))
